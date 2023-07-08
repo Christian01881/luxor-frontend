528c25af732a50d8b74bb397db4f5336d0152531
NicolaDiQuinzio <nicola.di.quinzio1@gmail.com>
2023-07-08 17:12:17 +0200

    add notfound and contact us pages

--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -1,4 +1,71 @@
 <template>
+<section class="p-5">
+<h2 class="h1-responsive font-weight-bold text-center my-4 title">Contattaci</h2>
+<p class="text-center w-responsive mx-auto mb-5">Hai delle domande? Non esitare a contattarci direttamente. Il nostro team ti risponderà entro poche ore per aiutarti.</p>
+
+<div class="row">
+
+    <div class="col-md-9 mb-md-0 mb-5">
+        <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="md-form mb-4">
+                         <label for="name" class="">Nome</label>
+                        <input type="text" id="name" name="name" class="form-control">
+                       
+                    </div>
+                </div>
+                
+
+                
+                <div class="col-md-6">
+                    <div class="md-form mb-4">
+                          <label for="email" class="">E-mail</label>
+                        <input type="text" id="email" name="email" class="form-control">
+                      
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+
+                
+                <div class="col-md-12">
+
+                    <div class="md-form mb-5">
+                        <label for="message">Il tuo messaggio</label>
+                        <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                        
+                    </div>
+
+                </div>
+            </div>
+
+        </form>
+
+        <div class="text-center text-md-left">
+            <a class="btn btn-warning " onclick="document.getElementById('contact-form').submit();">Invia</a>
+        </div>
+        <div class="status"></div>
+    </div>
+    
+    <div class="col-md-3 text-center">
+        <ul class="list-unstyled mb-0">
+            <li><i class="fas fa-map-marker-alt fa-2x title"></i>
+                <p>Milan, MI 20019 , ITALY</p>
+            </li>
+
+            <li><i class="fas fa-phone mt-4 fa-2x title"></i>
+                <p>+ 388 3478934</p>
+            </li>
+
+            <li><i class="fas fa-envelope mt-4 fa-2x title"></i>
+                <p>contact@gmail.com</p>
+            </li>
+        </ul>
+    </div>
+</div>
+
+</section>
 </template>
 
 <script>
@@ -8,5 +75,15 @@
 </script>
 
 <style lang="scss" scoped>
+section{
+   background-color:#05121b ;
+   color: white;
+
+}
+.title{
+    color: #fed200 ;
+}
+
+
 
 </style>
--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,4 +1,17 @@
 <template>
+    
+   <section>
+        <div class="d-flex align-items-center justify-content-center vh-100">
+            <div class="text-center">
+                <h1 class="display-1 fw-bold text-white">404</h1>
+                <p class="fs-3"> <span class="text-danger">Opps!</span> Pagina non trovata!</p>
+                <p class="lead">
+                    La pagina che stai cercando non esiste.
+                  </p>
+                <a href="index.html" class="btn btn-warning">Torna alla Home</a>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -8,5 +21,9 @@
 </script>
 
 <style lang="scss" scoped>
+section{
+   background-color:#05121b ;
+   color: white;
 
+}
 </style>
