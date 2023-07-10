da459984b7a007fa6ac650a0efaff1a36727a8c9
davidecaputo <daca9621@gmail.com>
2023-07-10 16:13:55 +0200

    add CardComponent, AboutUs and NotFound

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,12 +1,185 @@
 <template>
+  <div class="bg-black">
+    <div class="container py-5">
+      <div class="row h-100 align-items-center py-5">
+        <div class="col-lg-6">
+          <h1 class="display-4 title fw-bold">Chi siamo</h1>
+          <p class="lead text-white mb-0">Benvenuti su Luxorbnb, la piattaforma perfetta per trovare alloggi straordinari
+            in tutto il mondo. Siamo qui per rendere il tuo viaggio un'esperienza indimenticabile, offrendoti una vasta
+            selezione di alloggi di qualità superiore e un processo di prenotazione semplice e conveniente. Unisciti a noi
+            su Luxorbnb e scopri il modo perfetto per viaggiare.</p>
+        </div>
+        <div class="col-lg-6 d-none d-lg-block"><img src="https://bootstrapious.com/i/snippets/sn-about/illus.png" alt=""
+            class="img-fluid"></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="bg-dark py-5">
+    <div class="container py-5">
+      <div class="row align-items-center mb-5">
+        <div class="col-lg-6 order-2 order-lg-1"><i class="fa fa-bar-chart fa-2x mb-3 title"></i>
+          <h2 class="font-weight-light title fw-bold">Lorem ipsum dolor sit amet</h2>
+          <p class="font-italic text-white mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p><a href="#"
+            class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
+        </div>
+        <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src="https://www.habitante.it/wp-content/uploads/2022/07/10-idee-di-design-per-le-ville-moderne-Unsplash-foto-di-Avi-Werde-1.jpg" alt="" class="img-fluid mb-4 mb-lg-0 rounded-5"></div>
+      </div>
+      <div class="row align-items-center">
+        <div class="col-lg-5 px-5 mx-auto"><img src="https://www.romolini.com/userfiles/tipologia/41/B-villa-in-vendita-italia.jpg" alt=""
+            class="img-fluid mb-4 mb-lg-0 rounded-5"></div>
+        <div class="col-lg-6"><i class="fa fa-leaf fa-2x mb-3 title"></i>
+          <h2 class="font-weight-light title fw-bold">Lorem ipsum dolor sit amet</h2>
+          <p class="font-italic text-white mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p><a href="#"
+            class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="bg-black py-5">
+    <div class="container py-5">
+      <div class="row mb-4">
+        <div class="col-lg-5">
+          <h2 class="display-4 font-weight-light title fw-bold">Il nostro team</h2>
+          <p class="font-italic text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+      </div>
+
+      <div class="row text-center">
+        <!-- Team item-->
+        <div class="col-12 col-sm-6 col-md-4 mb-4">
+          <div class="bg-white rounded shadow-sm py-5 px-4"><img
+              src="https://bootstrapious.com/i/snippets/sn-about/avatar-4.png" alt="" width="100"
+              class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Emanuele Rosella Musico</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-github"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-linkedin"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-instagram"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-facebook"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <!-- End-->
+
+        <!-- Team item-->
+        <div class="col-12 col-sm-6 col-md-4 mb-4">
+          <div class="bg-white rounded shadow-sm py-5 px-4"><img
+              src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100"
+              class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Christian Paliotta Moretti</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-github"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-linkedin"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-instagram"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-facebook"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <!-- End-->
+
+        <!-- Team item-->
+        <div class="col-12 col-sm-6 col-md-4 mb-4">
+          <div class="bg-white rounded shadow-sm py-5 px-4"><img
+              src="https://bootstrapious.com/i/snippets/sn-about/avatar-2.png" alt="" width="100"
+              class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Matteo Aguiari</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-github"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-linkedin"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-instagram"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-facebook"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <!-- End-->
+
+        <!-- Team item-->
+        <div class="col-12 col-sm-6 col-md-4">
+          <div class="bg-white rounded shadow-sm py-5 px-4"><img
+              src="https://bootstrapious.com/i/snippets/sn-about/avatar-1.png" alt="" width="100"
+              class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Jorge Castillo Tenasoa</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-github"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-linkedin"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-instagram"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-facebook"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <!-- End-->
+
+        <!-- Team item-->
+        <div class="col-12 col-sm-6 col-md-4">
+          <div class="bg-white rounded shadow-sm py-5 px-4"><img
+              src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100"
+              class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Davide Caputo</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-github"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-linkedin"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-instagram"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-facebook"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <!-- End-->
+
+        <!-- Team item-->
+        <div class="col-12 col-sm-6 col-md-4">
+          <div class="bg-white rounded shadow-sm py-5 px-4"><img
+              src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100"
+              class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Nicola Di Quinzio</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-github"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-linkedin"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-instagram"></i></a></li>
+              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-facebook"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <!-- End-->
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
+export default {
 
-    }
+}
 </script>
 
 <style lang="scss" scoped>
+.social-link {
+  width: 30px;
+  height: 30px;
+  border: 1px solid #ddd;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #666;
+  border-radius: 50%;
+  transition: all 0.3s;
+  font-size: 0.9rem;
+}
 
-</style>
+.social-link:hover,
+.social-link:focus {
+  background: #ddd;
+  text-decoration: none;
+  color: #555;
+}
+
+// section{
+//    background-color:#05121b ;
+//    color: white;
+// }
+.title {
+  color: #fed200;
+}</style>
