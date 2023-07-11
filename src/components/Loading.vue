e890933b250fe42318ef04015e578a24955efad1
JorgeLuix <tec.jorge.29@gmai.com>
2023-07-11 11:17:59 +0200

    add loading

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-        <h1>LOADING...</h1>
+    <div class="loading-page">
+        <div class="loading-overlay">
+            <div class="loading-icon">
+                <i class="fa-solid fa-hurricane"></i>
+                <!-- <p>Loading...</p> -->
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,5 +16,40 @@
 </script>
 
 <style lang="scss" scoped>
+.loading-page {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.5); 
+}
 
+.loading-overlay {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation: fadeIn 0.3s ease-in-out forwards;
+}
+
+@keyframes fadeIn {
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+}
+
+.loading-icon {
+   
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 44px;
+    color: white;
+}
 </style>
