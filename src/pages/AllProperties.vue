ac1fa68418b8cd41b7df620ad5b16ed68786fa86
davidecaputo <daca9621@gmail.com>
2023-07-12 10:29:43 +0200

    modify AllProperties

--- src/pages/AllProperties.vue
+++ src/pages/AllProperties.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="container py-5">
         <div class="row">
-            <div class="col-6 mb-5" v-for="property in properties" :key="property.id">
+            <div class="col-12 col-md-6 mb-5" v-for="(property, index) in properties" :key="property.id">
                 <div class="card h-100">
-                    <img v-if="property.images.length > 0" :src="'http://127.0.0.1:8000/storage/' + property.images[0].path" alt="">
-                    <img v-else src="https://www.leonardospada.it/wp-content/uploads/2022/09/errore-404-pagina-non-trovata-1024x576.jpg" alt="" class="h-100">
+                    <img :src="`http://127.0.0.1:8000/storage/${property.images[0].path}`" alt="" class="card-img" v-if="property.images.length > 0">
+                    <img v-else src="https://www.leonardospada.it/wp-content/uploads/2022/09/errore-404-pagina-non-trovata-1024x576.jpg" alt="Image-Not-Found" class="h-100">
                     <div class="card-body">
                         <h4 class="fw-bold text-warning">
                             <router-link :to="{ name: 'single-property', params: { slug: property.slug } }">
@@ -36,6 +36,7 @@
     </div>
 </template>
 
+
 <script>
 import { store } from '../store.js';
 import axios from 'axios';
@@ -44,14 +45,14 @@ export default {
     data() {
         return {
             store,
-            properties: []
+            properties: [],
         };
     },
     methods: {
         getData() {
             axios.get(`${store.apiURL}/properties`).then((res) => {
                 console.log(res.data.results.data);
-                this.properties = res.data.results.data
+                this.properties = res.data.results.data;
             })
         }
     },
