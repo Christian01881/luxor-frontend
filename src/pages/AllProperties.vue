d818d9f8b1310e8f9338b5aae39cb7267ced58e3
NicolaDiQuinzio <nicola.di.quinzio1@gmail.com>
2023-07-12 13:37:52 +0200

    fix allproperties

--- src/pages/AllProperties.vue
+++ src/pages/AllProperties.vue
@@ -3,7 +3,7 @@
     <div class="m-5 d-flex justify-content-center align-items-center text-white">
     <div class="col-md-12">
       <label for="address" class="form-label text-uppercase fw-bold">Indirizzo *</label>
-      <div id="address">
+      <div id="address" class="bg-white">
        </div> 
        <div class="mt-3">
        <button @click="searchApartments()">Cerca appartamenti</button>
@@ -101,106 +101,6 @@
 </div>
 
 
-    <!-- <div class="container py-5">
-        <div class="row">
-            <div>
-		<div class="col-md-6">
-			<label for="address" class="form-label text-uppercase fw-bold">Indirizzo *</label>
-			<div id="address">
-				<button @click="searchApartments()">Cerca appartamenti</button>
-			</div>
-			<div>
-				<label for="beds">beds</label>
-				<select name="beds" id="beds">
-					<option value="1" >1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-					<option value="5">5</option>
-					<option value="6">6</option>
-					<option value="7">7</option>
-					<option value="8">8</option>
-					<option value="9">9</option>
-					<option value="10">10</option>
-				</select>
-			</div>
-			<div>
-				<label for="rooms">rooms</label>
-				<select name="rooms" id="rooms">
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-					<option value="5">5</option>
-					<option value="6">6</option>
-					<option value="7">7</option>
-					<option value="8">8</option>
-					<option value="9">9</option>
-					<option value="10">10</option>
-				</select>
-			</div>
-			<div>
-				<label for="radius">radius</label>
-				<select name="radius" id="radius">
-					<option value="20" default>20</option>
-					<option value="30">30</option>
-					<option value="40">40</option>
-					<option value="50">50</option>
-				</select>
-			</div>
-			<div>
-				<label for="services">services</label>
-				<select name="services" id="services">
-					<option value="">all results</option>
-					<option value="1">wifi</option>
-					<option value="2">Posto Macchina</option>
-					<option value="3">Piscina</option>
-					<option value="4">Portineria</option>
-					<option value="5">Sauna</option>
-					<option value="6"> Vista Mare</option>
-				</select>
-			</div>
-		</div>
-		<div>
-			<div class="w-50">
-				<div class="col-12 col-md-6 mb-5" v-for="(property, index) in properties" :key="property.id">
-                <div class="card h-100">
-                    <img :src="`http://127.0.0.1:8000/storage/${property.images[0].path}`" alt="" class="card-img" v-if="property.images.length > 0">
-                    <img v-else src="https://www.leonardospada.it/wp-content/uploads/2022/09/errore-404-pagina-non-trovata-1024x576.jpg" alt="Image-Not-Found" class="h-100">
-                    <div class="card-body">
-                        <h4 class="fw-bold text-warning">
-                            <router-link :to="{ name: 'single-property', params: { id: property.id } }">
-                                {{ property.title }}
-                            </router-link>
-                        </h4>
-                        <div class="row">
-                            <div class="col-4">
-                                <p><span class="fw-bold"><i class="fa-solid fa-bed"></i> Camere: </span>{{ property.rooms }}</p>
-                            </div>
-                            <div class="col-4">
-                                <p><span class="fw-bold"><i class="fa-solid fa-sink"></i> Bagni: </span>{{ property.bathrooms }}</p>
-                            </div>
-                            <div class="col-4">
-                                <p><span class="fw-bold"><i class="fa-solid fa-ruler-combined"></i> Metri quadri: </span>{{ property.square_meters }}</p>
-                            </div>
-                            <div class="col-6 text-center">
-                                <p><span class="fw-bold"><i class="fa-solid fa-map-location"></i> Indirizzo: </span>{{ property.address }}</p>
-                            </div>
-                            <div class="col-6 text-center">
-                                <p><span class="fw-bold "><i class="fa-solid fa-bell-concierge"></i> Servizi: </span><span v-for="service in property.services" class="me-2">{{ service.name }}</span></p>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-			</div>
-		</div>
-	
-
-            
-        </div>
-    </div></div>*/ -->
 </template>
 
 
