a9638630c6a46c59d327e4fd68e419dac962dcb2
JorgeLuix <tec.jorge.29@gmai.com>
2023-07-12 11:53:58 +0200

    df

--- src/pages/AllProperties.vue
+++ src/pages/AllProperties.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container py-5">
         <div class="row">
-            
+
             <div class="col-12 col-md-6 mb-5" v-for="(property, index) in properties" :key="property.id">
                 <div class="card h-100">
                     <img :src="`http://127.0.0.1:8000/storage/${property.images[0].path}`" alt="" class="card-img" v-if="property.images.length > 0">
@@ -34,6 +34,7 @@
                 </div>
             </div>
         </div>
+    </div>
 </template>
 
 
